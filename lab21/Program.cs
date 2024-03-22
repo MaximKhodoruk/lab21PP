1c45a4734f48f531416a3bb8e3768153cc3843fa
MaximKhodoruk <maksimiliankhodoruk@mail.ru>
2024-03-22 23:41:18 +0300

    lab21pp

---
+++ lab21/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите действие:\n1 - ввести А\n2 - ввести В\n3 - выполнить операцию +\n4 - выполнить операцию -\n5 - выполнить операцию *\n6 - выполнить операцию /\n7 - закончить программу");
+        }
+    }
+}
